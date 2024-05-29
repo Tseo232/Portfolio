628dd3d90b981bed3972c2320a8b2e7abe5e8c8a
Tseo232 <113905825+Tseo232@users.noreply.github.com>
2024-05-29 17:43:10 +0200

    portfolio

--- Js/main.js
+++ Js/main.js
@@ -1,7 +1,5 @@
-
 // src/index.js
 import * as THREE from 'three';
-
 // Create the scene
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
